8bcbfae0489c08451a5019044fbbea69965d8f19
macsbaklanoff <baklanov689@gmail.com>
2024-12-05 17:34:50 +0300

    add select elements and change states items

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './styles.scss'
+
+const blocks = document.querySelectorAll(".navigation-driver__main__item-main")
+let activeBlock = null
+activeBlock = blocks[0]
+activeBlock.classList.add('item-active')
+
+blocks.forEach(block => {
+    block.addEventListener('click', () => {
+        if (activeBlock) {
+            activeBlock.classList.remove('item-active')
+        }
+        block.classList.add('item-active')
+        activeBlock = block
+    });
+});
